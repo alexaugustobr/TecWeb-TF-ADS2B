290459b68dbc23e777b244f6b57b08240f85cfe8
Michaelads <maike_maicon09@hotmail.com>
2017-10-24 21:48:55 -0200

    push

--- core/views/cursos.py
+++ core/views/cursos.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from core.models.Curso import Curso
 
 def cursos (request):
-    return render(request,"cursos/cursos.html")
+
+    contexto = {
+        "usuario":"usuario",
+        "cursos":Curso.objects.all()
+    }
+    return render(request,"cursos/cursos.html",contexto)
 
 def bancoDados (request):
     return render(request,"cursos/cursoBancoDados.html")
