b355de9724447a51c29e6d4d696a9f244d47b2a3
alexcologia33 <alexcologia33@gmail.com>
2017-11-09 21:10:51 -0200

    db

--- core/submodels/Matricula.py
+++ core/submodels/Matricula.py
@@ -1,11 +1,8 @@
 from django.db import models
 
 class Matricula(models.Model):
+    turma = models.ForeignKey(to='Turma', related_name="matriculas", null=False, blank=False) #onetomany
     aluno = models.ForeignKey(to='Aluno', related_name="matriculas", null=False, blank=False) #onetomany
-    nome = models.CharField(max_length=120,null=False)
-    email = models.CharField(max_length=80)
-    celular = models.CharField(max_length=11)
-    ra = models.IntegerField(unique=True,null=False)
     
     class Meta:
         db_table = 'Matricula'
--- core/submodels/Resposta.py
+++ core/submodels/Resposta.py
@@ -1,9 +1,12 @@
 
 from django.db import models
 from .Curso import Curso
+from .Aluno import Aluno
+from .Questao import Questao
 
 class Reposta(models.Model):
-    aluno = models.ForeignKey(to='Aluno', related_name="matriculas", null=False, blank=False) #onetomany
+    aluno = models.ForeignKey(to='Aluno', related_name="respostas", null=False, blank=False) #onetomany
+    questao = models.ForeignKey(to='Questao', related_name="respostas", null=False, blank=False) #onetomany
     data_avaliacao = models.DateField()
     nota = models.DecimalField(max_digits=4,decimal_places=2)
     avaliacao = models.TextField()
