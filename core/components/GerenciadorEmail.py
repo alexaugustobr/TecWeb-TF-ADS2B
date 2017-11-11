0d8771c22cfb2f29633f4406f02c2cab124b2a56
alexcologia33 <alexcologia33@gmail.com>
2017-11-11 19:20:15 -0200

    init

---
+++ core/components/GerenciadorEmail.py
@@ -0,0 +1,22 @@
+
+class GerenciadorEmail:
+    
+    def __init__(self):
+        self.__carregarConfig()
+
+    def __carregarConfig(self):
+
+
+    def enviarEmail(self, destinatario, rementente, assunto):
+   asds
+
+
+
+def main():
+    email = GerenciadorEmail():
+
+
+
+
+if "__name__" == "__main__":
+    main()
