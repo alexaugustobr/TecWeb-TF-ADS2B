33e87c8b622471c73a2f3c502d083da44929e2be
alexcologia33 <alexcologia33@gmail.com>
2017-11-12 17:02:07 -0200

    sql teste

--- testes/test_TesteMatricula.py
+++ testes/test_TesteMatricula.py
@@ -4,8 +4,8 @@ from core.models import *
 class TesteMatricula(TestCase):
     def setUp(self):
         curso = Curso()
-        curso.nome = "ADS"
-        curso.sigla = "ADS"
+        curso.nome = "SI"
+        curso.sigla = "SI"
         curso.save()
 
         aluno = Aluno()
@@ -14,13 +14,68 @@ class TesteMatricula(TestCase):
         aluno.ra = 12356
         aluno.save()
 
-        aluno = Aluno()
-        aluno.curso = curso
-        aluno.nome = "Dani"
-        aluno.ra = 1355546
-        aluno.save()
+        aluno2 = Aluno()
+        aluno2.curso = curso
+        aluno2.nome = "Dani"
+        aluno2.ra = 1355546
+        aluno2.save()
 
+        curso = Curso()
 
+        curso.sigla = "ADS"
+        curso.nome = "Analise e desenv de sistemas"
+
+        curso.save()
+
+        gc = GradeCurricular()
+        gc.curso = curso
+        gc.ano = 2017
+        gc.semestre = "P"
+        gc.save()
+
+        p = Periodo()
+        p.numero = 1
+        p.ano_grade = 2017
+        p.gradeCurricular = gc
+        p.save()
+
+        d = Disciplina()
+        d.nome = "TECWEB"
+        d.carga_horaria = 10
+        d.teoria = 5
+        d.pratica = 5
+        d.save()
+
+        pd = PeriodoDisciplina()
+        pd.periodo = p
+        pd.disciplina = d
+        pd.save()
+
+        do = DisciplinaOfertada()
+        do.disciplina = d
+        do.ano = 2017
+        do.semestre = "P"
+        do.save()
+
+        p = Professor()
+        p.nome = "Lima"
+        p.apeido = "Lima"
+        p.ra = 123123
+        p.email = "lima@impacta.edu.br"
+        p.celular = 12345678910
+        p.save()
+
+        t = Turma()
+        t.sigla_turma = "A"
+        t.turno = "Manha"
+        t.professor = p
+        t.disciplinaOfertada = do
+        t.save()
+
+        m = Matricula()
+        m.aluno = aluno2
+        m.turma = t
+        m.save()
 
 
     def test_aluno_cadastrado(self):
@@ -31,6 +86,12 @@ class TesteMatricula(TestCase):
         alunos = Aluno.objects.all()
         self.assertEqual(len(alunos), 2)
 
-    def test_aluno_matriculado(self):
-        alunos = Aluno.objects.raw("SELECT * FROM ALUNO LEFT JOIN MATRICULA WHERE ON ALUNO.ID = MATRICULA.ALUNO_ID WHERE MATRICULA.ID IS NULL")
-        self.assertEqual(len(alunos), 2)
+    def test_aluno_nao_matriculados(self):
+        alunos = Aluno.objects.raw('SELECT * FROM ALUNO LEFT JOIN MATRICULA ON ALUNO.ID = MATRICULA.ALUNO_ID WHERE MATRICULA.ID IS NULL')
+        for aluno in alunos:
+            self.assertEqual(aluno.nome, "Alex")
+            
+    def test_aluno_matriculados(self):
+        alunos = Aluno.objects.raw('SELECT * FROM ALUNO INNER JOIN MATRICULA ON ALUNO.ID = MATRICULA.ALUNO_ID')
+        for aluno in alunos:
+            self.assertEqual(aluno.nome, "Dani")
