59aa4e20246f61028ba73281f246b506569cc052
alexcologia33 <alexcologia33@gmail.com>
2017-11-12 21:01:54 -0200

    email

--- professor/views/matricula.py
+++ professor/views/matricula.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from core.models.Turma import Turma
+from core.models import *
 from datetime import datetime
-
+from django.core.serializers import serialize
+from core.components.GerenciadorEmail import Email
 from django.http import HttpResponse
 
 
@@ -18,16 +19,43 @@ def turma(request):
 
 
 def enviarEmailTurma(request):
-    if request.method == 'POST':
-        turma_id = request.POST.get('turma_id')
-        if turma_id:
-            turma = Turma.objects.get(id=turma_id)
+    if request.method != 'POST':
+        return HttpResponse(status=403) 
 
-            #TODO
-            #gerar token
-            token = 123456 # =)
-            
-            return HttpResponse(status=200)
+    turma_id = request.POST.get('turma_id')
 
+    if turma_id == None:
+        return HttpResponse(status=403)
+        
+    turma = Turma.objects.get(id=turma_id)
 
-    return HttpResponse(status=403)
+    if turma == None:
+        return HttpResponse(status=403)
+
+    link = "localhost:8000/aluno/turma/{}/matricular/".format(turma_id)
+    
+    disciplina = turma.disciplinaOfertada.disciplina
+    professor = turma.professor
+
+    alunos = Aluno.objects.raw('SELECT * FROM ALUNO LEFT JOIN MATRICULA ON ALUNO.ID = MATRICULA.ALUNO_ID WHERE MATRICULA.ID IS NULL')
+    
+    for aluno in alunos:
+        #TODO
+        #gerar token
+        token = 123456 # =)
+        contexto = {
+            "aluno":aluno, 
+            "professor":professor, 
+            "disciplina":disciplina,
+            "token":token, 
+            "link":link
+        }
+        email = Email("contato@handcode.com", "Faculdade Handcode - matricula para a disciplina de {}".format(disciplina.nome))
+        email.html("emails/contatoFaculdade.html", contexto)
+        email.enviar(aluno.email)
+
+    contexto = {
+        "alunos": alunos
+    }
+    
+    return HttpResponse(status=200)
