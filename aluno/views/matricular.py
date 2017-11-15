7b61ce2735e3115110933539eef2d4dcd0ebae46
alexcologia33 <alexcologia33@gmail.com>
2017-11-15 20:10:52 -0200

    token

--- aluno/views/matricular.py
+++ aluno/views/matricular.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from core.components import *
+from core.models import *
+
 def matricular (request):
     return render(request,"matricula/formMatricula.html")
 
-def confirmar (request):
+def confirmarMatricula (request):
 
+    codigo = request.GET.get('codigo')
 
+    if not type(codigo) == str:
+        return HttpResponse(status=400)
 
-    return render(request,"matricula/formMatricula.html")
+    g = GerenciadorToken()
+    token = g.traduzir(codigo)
+
+    if not g.autenticar(token):
+        return HttpResponse(status=401)
+
+    return HttpResponse(status=200)
