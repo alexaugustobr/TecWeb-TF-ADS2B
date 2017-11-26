2af7d4a203a710650ae3134ee100c78a009c0f9e
alexcologia33 <alexcologia33@gmail.com>
2017-11-26 14:10:29 -0200

    fix boletim

--- aluno/views/boletim.py
+++ aluno/views/boletim.py
@@ -4,3 +4,9 @@ from core.components import *
 from core.models import *
 import datetime
 from django.contrib.auth.decorators import login_required, user_passes_test
+
+@login_required(login_url='/login')
+@user_passes_test(lambda user: user.perfil == 'A', login_url='/login?error=acesso', redirect_field_name=None)
+def boletim (request):
+
+    return render(request,"boletim/boletim.html", contexto)
